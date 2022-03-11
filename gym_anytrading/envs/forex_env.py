2117fdbfa2f10d754079286da4001e01641d6992
Gleb Khizhnyak <Gleb.Khizhnyak1989@gmail.com>
2022-03-11 21:12:52 +0300

    Add pandas v2

--- gym_anytrading/envs/forex_env.py
+++ gym_anytrading/envs/forex_env.py
@@ -24,7 +24,7 @@ class ForexEnv(TradingEnv):
         prices = prices[self.frame_bound[0]-self.window_size:self.frame_bound[1]]
 
         diff = np.insert(np.diff(prices), 0, 0)
-        signal_features = np.column_stack((pd.concat([(self.df.iloc[:,:4]-self.df.iloc[:,:4].mean())/self.df.iloc[:,:4].std(), (self.df.iloc[:,4]-self.df.iloc[:,4].mean())/self.df.iloc[:,4].std()], axis=1).to_numpy()))
+        signal_features = (pd.concat([(self.df.iloc[:,:4]-self.df.iloc[:,:4].mean())/self.df.iloc[:,:4].std(), (self.df.iloc[:,4]-self.df.iloc[:,4].mean())/self.df.iloc[:,4].std()], axis=1).to_numpy())
 
         return prices, signal_features
 
