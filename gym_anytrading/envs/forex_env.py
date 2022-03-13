82ce2c522bbe14d3c0072f4d91ed181c361f0504
Gleb Khizhnyak <Gleb.Khizhnyak1989@gmail.com>
2022-03-13 12:34:09 +0300

    Исправил ошибку в строке 23

--- gym_anytrading/envs/forex_env.py
+++ gym_anytrading/envs/forex_env.py
@@ -29,6 +29,7 @@ class ForexEnv(TradingEnv):
 
         back_data[self.frame_bound[0] - self.window_size]
         back_data = back_data[self.frame_bound[0]-self.window_size:self.frame_bound[1]]
+        print(prices, back_data)
 
 
         diff = np.insert(np.diff(prices), 0, 0)
